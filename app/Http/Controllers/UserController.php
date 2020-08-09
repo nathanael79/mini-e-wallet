60848f3355766df25f81de70bfbe84ee0df484dc
Imanuel Ronaldo <imanuelronaldo@gmail.com>
2020-08-09 10:51:23 +0700

    feature-rest-crud-user

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,111 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+
+use App\User;
+use Exception;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function createUser(Request $request){
+        $this->validate($request,[
+            'username' => 'required',
+            'email' => 'required|unique:users',
+            'password' => 'required|min:6'
+        ]);
+
+        try{
+            $data = User::create(array_merge($request->all(),['password' => Hash::make($request->password)]));
+            return $this->getResponse('data created', $data, 201);
+        }catch (Exception $e){
+            return $this->getResponse($e->getMessage(), '', 500);
+        }
+    }
+
+    public function updateUser($id, Request $request){
+        $user = User::find($id);
+        if(is_null($user)){
+            return $this->getResponse('user not found', '', 404);
+        }else{
+            $this->validate($request,[
+                'username' => 'required',
+                'email' => 'required|unique:users',
+                'password' => 'required|min:6'
+            ]);
+
+            try{
+                $user->update($request->all());
+                return $this->getResponse('user updated', $user , 200);
+            }catch (Exception $e){
+                return $this->getResponse($e->getMessage(), '', 500);
+            }
+        }
+    }
+
+    public function getUsers(){
+        $data = User::all();
+
+        if($data->isEmpty()){
+            return $this->getResponse('data is empty', '', 404);
+        }else{
+            return $this->getResponse('data found', $data, 200);
+        }
+    }
+
+    public function getUserById($id){
+        $data = User::find($id);
+
+        if($data == null){
+            return $this->getResponse('data not found', '', 404);
+        }else{
+            return $this->getResponse('data found', $data, 200);
+        }
+    }
+
+    public function getUsersWithBalance(){
+        $data = User::with('usersBalance')->get();
+
+        if($data->isEmpty()){
+            return $this->getResponse('data is empty', '', 404);
+        }else{
+            return $this->getResponse('data found', $data, 200);
+        }
+    }
+
+    public function getUserByIdWithBalance($id){
+        $data = User::with('usersBalance')->find($id);
+
+        if($data == null){
+            return $this->getResponse('data not found', '', 404);
+        }else{
+            return $this->getResponse('data found', $data, 200);
+        }
+    }
+
+    public function deleteUser($id){
+        $data = User::find($id);
+
+        if(is_null($data)){
+            return $this->getResponse('data not found', '', 404);
+        }else{
+            try{
+                $data->delete();
+                return $this->getResponse('data is deleted', '', 200);
+            }catch (Exception $e){
+                return $this->getResponse($e->getMessage(), '', 500);
+            }
+        }
+    }
+
+    private function getResponse($message = '', $data = '', $code = 200){
+        return response()->json([
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+}
