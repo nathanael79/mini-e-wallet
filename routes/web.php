0db311e59c4bc50de08808522e8970ade8d24d83
Imanuel Ronaldo <imanuelronaldo@gmail.com>
2020-08-09 10:51:38 +0700

    feature-add-routes-rest-user

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,17 @@
 |
 */
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
+// API route group
+$router->group(['prefix' => 'api/v1'], function () use ($router) {
+    $router->group(['prefix' => 'users'], function () use ($router){
+        $router->get('/with-balance', 'UserController@getUsersWithBalance');
+        $router->get('/with-balance/{id}', 'UserController@getUserByIdWithBalance');
+        $router->get('/', 'UserController@getUsers');
+        $router->get('/{id}', 'UserController@getUserById');
+        $router->post('/','UserController@createUser');
+        $router->put('/{id}', 'UserController@updateUser');
+        $router->delete('/{id}', 'UserController@deleteUser');
+    });
 });
 
 $router->get('/key', function() {
