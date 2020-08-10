f5611362dfd513278c72126e203733bb46bbdc18
Imanuel Ronaldo <imanuelronaldo@gmail.com>
2020-08-10 19:48:41 +0700

    feature-fixing-balance-bank-history-table-migration

--- database/migrations/2020_08_08_151315_create_blance_bank_history_table.php
+++ database/migrations/2020_08_08_151315_create_blance_bank_history_table.php
@@ -16,15 +16,15 @@ class CreateBlanceBankHistoryTable extends Migration
         Schema::create('blance_bank_history', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('balance_bank_id');
-            $table->foreign('balance_bank_id')->references('id')->on('blance_bank');
+            $table->foreign('balance_bank_id')->references('id')->on('blance_bank')->onDelete('cascade');
             $table->integer('balance_before');
             $table->integer('balance_after');
             $table->string('activity');
             $table->enum('type',['credit','debit']);
-            $table->string('ip');
-            $table->string('location');
-            $table->string('user_agent');
-            $table->string('author');
+            $table->string('ip')->nullable();
+            $table->string('location')->nullable();
+            $table->string('user_agent')->nullable();
+            $table->string('author')->nullable();
             $table->timestamps();
         });
     }
