2cfd24300fccb77cc63e32721cc078fbd569633f
Imanuel Ronaldo <imanuelronaldo@gmail.com>
2020-08-10 19:50:46 +0700

    feature-add-return-response-json

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    //
+    protected function respondWithToken($token){
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => Auth::factory()->getTTL() * 60
+        ], 200);
+    }
+
+    protected function getResponse($message = '', $data = '', $code = 200){
+        return response()->json([
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
 }
