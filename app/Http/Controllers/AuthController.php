8e505781e2720ecedff7c3296f14c14eb389fbc9
Imanuel Ronaldo <imanuelronaldo@gmail.com>
2020-08-10 19:49:58 +0700

    feature-login-logout-jwt

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,37 @@
 namespace App\Http\Controllers;
 
 
-class AuthController
-{
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
+class AuthController extends Controller
+{
+    public function login(Request $request){
+        //validate incoming request
+        $this->validate($request, [
+            'email' => 'required|string',
+            'password' => 'required|min:6',
+        ]);
+
+        $credentials = $request->only(['email', 'password']);
+
+        if (! $token = Auth::attempt($credentials)) {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
+
+        return $this->respondWithToken($token);
+    }
+
+    public function logout() {
+        JWTAuth::invalidate(JWTAuth::getToken());
+
+        return response()->json(
+            [
+                'status' => 'ok',
+                "code" => 200,
+                "message"=> ""
+            ], 200
+        );
+    }
 }
