c0b98d740ee347608b7ef9c21d4a06ea616f6e77
Imanuel Ronaldo <imanuelronaldo@gmail.com>
2020-08-10 19:52:12 +0700

    feature-add-user-balance-rest-api

--- app/Http/Controllers/UserBalanceHistoryController.php
+++ app/Http/Controllers/UserBalanceHistoryController.php
@@ -4,7 +4,34 @@
 namespace App\Http\Controllers;
 
 
-class UserBalanceHistoryController
-{
+use App\UsersBalanceHistory;
 
+class UserBalanceHistoryController extends Controller
+{
+    public function getBalanceHistoryAll(){
+        $data = UsersBalanceHistory::all();
+        if($data->isEmpty()){
+            return $this->getResponse('data is empty', $data, 404);
+        }else{
+            return $this->getResponse('data found', $data, 200);
+        }
+    }
+
+    public function getBalanceHistoryById($id){
+        $data = UsersBalanceHistory::find($id);
+        if(is_null($data)){
+            return $this->getResponse('data is empty', $data, 404);
+        }else{
+            return $this->getResponse('data found', $data, 200);
+        }
+    }
+
+    public function getBalanceHistoryByUserBalanceId($userBalanceId){
+        $data = UsersBalanceHistory::where('user_balance_id',$userBalanceId)->get();
+        if(is_null($data)){
+            return $this->getResponse('data is empty', $data, 404);
+        }else{
+            return $this->getResponse('data found', $data, 200);
+        }
+    }
 }
