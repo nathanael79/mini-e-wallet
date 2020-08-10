25122505a8ebfad4780c2c58c0cd1dd997c828da
Imanuel Ronaldo <imanuelronaldo@gmail.com>
2020-08-10 19:49:44 +0700

    feature-add-config-for-jwt

--- app/User.php
+++ app/User.php
@@ -7,8 +7,9 @@ use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Lumen\Auth\Authorizable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Model implements AuthenticatableContract, AuthorizableContract
+class User extends Model implements AuthenticatableContract, AuthorizableContract, JWTSubject
 {
     use Authenticatable, Authorizable;
 
@@ -28,5 +29,15 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
 
     public function usersBalance(){
         return $this->hasMany(UsersBalance::class);
+    }
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims()
+    {
+        return [];
     }
 }
