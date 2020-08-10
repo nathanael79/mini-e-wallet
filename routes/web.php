b0d2a6f023db1177a1bc6dd93afc99ecd912e4ef
Imanuel Ronaldo <imanuelronaldo@gmail.com>
2020-08-10 19:52:52 +0700

    feature-fixing-routes

--- routes/web.php
+++ routes/web.php
@@ -13,21 +13,40 @@
 
 // API route group
 $router->group(['prefix' => 'api/v1'], function () use ($router) {
-    $router->group(['prefix' => 'users'], function () use ($router){
-        $router->get('/with-balance', 'UserController@getUsersWithBalance');
-        $router->get('/with-balance/{id}', 'UserController@getUserByIdWithBalance');
-        $router->get('/', 'UserController@getUsers');
-        $router->get('/{id}', 'UserController@getUserById');
-        $router->post('/','UserController@createUser');
-        $router->put('/{id}', 'UserController@updateUser');
-        $router->delete('/{id}', 'UserController@deleteUser');
-    });
 
-    $router->group(['prefix' => 'user-balance'], function () use ($router){
-        $router->get('/','UserBalanceController@getAllBalance');
-        $router->get('/{id}','UserBalanceController@getBalanceById');
-        $router->post('/','UserBalanceController@createBalance');
-        $router->delete('/{id}','UserBalanceController@deleteBalance');
+    // Matches "/api/v1/login
+    $router->post('login', 'AuthController@login');
+
+    $router->group(['middleware' => 'jwt.auth'], function () use ($router) {
+
+        $router->group(['prefix' => 'users'], function () use ($router){
+            $router->get('/with-balance', 'UserController@getUsersWithBalance');
+            $router->get('/with-balance/{id}', 'UserController@getUserByIdWithBalance');
+            $router->get('/', 'UserController@getUsers');
+            $router->get('/{id}', 'UserController@getUserById');
+            $router->post('/','UserController@createUser');
+            $router->put('/{id}', 'UserController@updateUser');
+            $router->delete('/{id}', 'UserController@deleteUser');
+        });
+
+        $router->group(['prefix' => 'user-balance'], function () use ($router){
+            $router->get('/','UserBalanceController@getAllBalance');
+            $router->get('/{id}','UserBalanceController@getBalanceById');
+            $router->post('/','UserBalanceController@createBalance');
+            $router->post('/transfer','UserBalanceController@transferToBank');
+            $router->post('/retrieve','UserBalanceController@transferFromBank');
+            $router->delete('/{id}','UserBalanceController@deleteBalance');
+        });
+
+        $router->group(['prefix' => 'bank-balance'], function () use ($router){
+            $router->get('/','BalanceBankController@getAllBalance');
+            $router->get('/{id}','BalanceBankController@getBalanceById');
+            $router->post('/','BalanceBankController@createBalance');
+            $router->delete('/{id}','BalanceBankController@deleteBalance');
+        });
+
+        $router->post('/transfer-to-bank','UserBalanceController@transferToBank');
+        $router->post('/logout', 'AuthController@logout');
     });
 });
 
