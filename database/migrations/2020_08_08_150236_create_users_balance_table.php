d3cd58c22ba38200086aef12558b1dc7a3388ced
Imanuel Ronaldo <imanuelronaldo@gmail.com>
2020-08-10 19:47:10 +0700

    feature-fixing-users-balance-table-migration

--- database/migrations/2020_08_08_150236_create_users_balance_table.php
+++ database/migrations/2020_08_08_150236_create_users_balance_table.php
@@ -16,7 +16,7 @@ class CreateUsersBalanceTable extends Migration
         Schema::create('users_balance', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->integer('balance');
             $table->integer('balance_achieve');
             $table->timestamps();
